6b962e89dd9cc3944ea4eb13f64e1fa1a60178ae
Márk Mezei <94128834+markmezei@users.noreply.github.com>
2023-09-25 01:45:09 +0200

    Add files via upload

--- DeleteTempFolder.ps1
+++ DeleteTempFolder.ps1
@@ -6,10 +6,10 @@ if (test-path $TempFolder){
     ri -path $TempFolder\* -recurse -force -errorAction SilentlyContinue
     $afterSize = (gci $TempFolder -recurse | measure -property Length -sum).Sum
     
-    $deletedMB = [System.Math]::Round(($beforeSize - $afterSize) / 1MB, 2)
-    $deletedGB = [System.Math]::Round(($beforeSize - $afterSize) / 1GB, 2)
+    $deletedMB = [System.Math]::Round(($beforeSize - $afterSize) / 1MB, 4)
+    $deletedGB = [System.Math]::Round(($beforeSize - $afterSize) / 1GB, 4)
 
-    if($deletedGB -gt 0 -or $deletedMB -gt 0){
+    if($deletedGB -gt 0 -and $deletedMB -gt 0){
         echo "Temp folder deleted successfully."
         echo "Freed up $deletedGB GB ($deletedMB MB) of disk space"
     }
