e94b9117932fb629a0b8adf0b6c49ba58d8b4204
Márk Mezei <94128834+markmezei@users.noreply.github.com>
2023-09-25 01:20:13 +0200

    Add files via upload

---
+++ DeleteTempFolder.ps1
@@ -0,0 +1,24 @@
+﻿$AppData = [System.Environment]::GetFolderPath('LocalApplicationData')
+$TempFolder = join-path $AppData 'Temp'
+
+if(test-path $TempFolder){
+    if (test-path $TempFolder){
+        $beforeSize = (gci $TempFolder -recurse | measure -property Length -sum).Sum
+        ri -path $TempFolder\* -recurse -force -errorAction SilentlyContinue
+        $afterSize = (gci $TempFolder -recurse | measure -property Length -sum).Sum
+        
+        $deletedMB = [System.Math]::Round(($beforeSize - $afterSize) / 1MB, 2)
+        $deletedGB = [System.Math]::Round(($beforeSize - $afterSize) / 1GB, 2)
+
+        if($deletedGB -or $deletedMB -gt 0){
+            echo "Temp folder deleted successfully."
+            echo "Freed up $deletedGB GB ($deletedMB MB) of disk space"
+        }
+        else{
+            echo "No disk space was freed up"
+        }    
+    }   
+    else{
+        echo "Temp folder not found"
+    }
+}
